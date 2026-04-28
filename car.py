c5bc6ac53e447ae531144d03149fca228a8cda0a
Sofia Rodriguez <snd998.sr@gmail.com>
2026-04-28 11:38:13 -0500

    i

--- car.py
+++ car.py
@@ -1,2 +1,15 @@
-# Write your class definition here!
+class Car:
+    def __init__(self, car_id, brand, year, color, mileage=0.0):
+        self.car_id = car_id
+        self.brand = brand
+        self.year = year
+        self.color = color
+        self.mileage = mileage
 
+    def change_color(self, new_color):
+        self.color = new_color
+
+    def drive(self, miles):
+        self.mileage += miles
+
+    
