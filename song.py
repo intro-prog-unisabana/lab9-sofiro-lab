aca2d703d12758c1679563834a02cf5057349fd4
Sofia Rodriguez <snd998.sr@gmail.com>
2026-04-28 11:32:25 -0500

    f

--- song.py
+++ song.py
@@ -5,3 +5,6 @@ class Song:
         self.length = length  
     def get_length_in_seconds(self):
         return self.length * 60
+    
+    def __str__(self):
+        return f"'{self.name}' by {self.artist} ({self.length})"
