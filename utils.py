5f1cce4e19093a3f40a2a9471f6fff83197f47e0
Sofia Rodriguez <snd998.sr@gmail.com>
2026-04-28 11:41:30 -0500

    s

--- utils.py
+++ utils.py
@@ -1,1 +1,14 @@
 # utils.py
+from car import Car
+
+def create_car_from_input():
+    car_id = input("Enter car ID (e.g., CAR001):\n")
+    brand = input("Enter car brand:\n")
+    year = int(input("Enter car year:\n"))
+    color = input("Enter car color:\n")
+    mileage = float(input("Enter mileage:\n"))
+    return Car(car_id, brand, year, color, mileage)
+
+def display_cars(car_dict):
+    for car in car_dict.values():
+        print(car)
