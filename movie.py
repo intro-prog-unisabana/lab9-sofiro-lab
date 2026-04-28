95ae562b67633e57c2325cc2a9257ce1754fd7a6
Sofia Rodriguez <snd998.sr@gmail.com>
2026-04-28 11:26:01 -0500

    o

--- movie.py
+++ movie.py
@@ -7,7 +7,8 @@ class Movie:
         self.year = year
 # FREEZE CODE END
     # TODO: Define the __str__ method!
-
+    def __str__(self):
+        return f"Movie: {self.title} (Directed by {self.director}, {self.year})"
 
 # FREEZE CODE BEGIN
 if __name__ == "__main__":
@@ -19,4 +20,6 @@ if __name__ == "__main__":
     
     
     # TODO: Construct a Movie object!
+    movie = Movie(title, director, year)
     # TODO: Print the object!
+    print(movie)
