388c8c2eeaf70e34833da710a07db2431dfde342
Sofia Rodriguez <snd998.sr@gmail.com>
2026-05-03 20:38:30 -0500

    ok

--- aircraft_altitude.py
+++ aircraft_altitude.py
@@ -23,7 +23,7 @@ def main():
         elif action.upper() == "D":
             aircraft.descend(feet)
 
-    print(f"Final altitude: {aircraft.get_altitude()} feet")
+    print(aircraft.altitude)
 
 
 if __name__ == "__main__":
